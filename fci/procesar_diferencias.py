a6544611222dce356b7d211d31d8fdf4c900b6f9
rodrigo <rdr.rkh+os@gmail.com>
2017-09-14 11:14:07 -0300

    Nueva funcionalidad para generar exportaciones en formato json
     Su rama está actualizada con «origin/master».
    
     Cambios para hacer commit:
    	modificado: fci/procesar_diferencias.py

--- fci/procesar_diferencias.py
+++ fci/procesar_diferencias.py
@@ -6,6 +6,8 @@ import sys
 import csv
 import argparse
 import time
+import json
+
 
 def getHoja(ruta):
 	archivo = open(ruta)
@@ -24,7 +26,8 @@ def get_fondos(lector):
 				indice = 5 if len(fila[4]) == 0 else 4
 				rta[fila[1]] = ('u$s' if len(fila[4]) == 0 else '$',
 								float(corregir_formato_monto(fila[3])),
-								float(corregir_formato_monto(fila[indice])))
+								float(corregir_formato_monto(fila[indice])),
+								float(corregir_formato_monto(fila[2])))
 	return rta
 
 def procesar(cvs_actual, cvs_anterior):
@@ -39,9 +42,13 @@ def procesar(cvs_actual, cvs_anterior):
 	rta = []
 	for clave in claves:
 		valor_previo = 0
+		cuotapartes_anteriores = 0
+		precio_previo = 0
 		
 		if clave in fondo_previo.keys():
 			valor_previo = fondo_previo[clave][2]
+			cuotapartes_anteriores = fondo_previo[clave][3]
+			precio_previo = fondo_previo[clave][1]
 		
 		rta.append((clave,
 					fondo_actual[clave][0],
@@ -51,14 +58,20 @@ def procesar(cvs_actual, cvs_anterior):
 					fondo_actual[clave][0],
 					valor_previo,
 					fondo_actual[clave][0],
-					fondo_actual[clave][2]-valor_previo))
+					fondo_actual[clave][2]-valor_previo,
+					fondo_actual[clave][3],
+					cuotapartes_anteriores,
+					precio_previo))
 	
 	return rta
 
 def get_output_string(unProcesado):
-	return "{}:\n {} {:f} => {} {:f} - {} {:f} = {} {:f}\n".format(unProcesado[0],
+	return "{} ({} << {}):\n {} {:f} [{:f}] => {} {:f} - {} {:f} = {} {:f}\n".format(unProcesado[0],
+																		unProcesado[9],
+																		unProcesado[10],
 																		unProcesado[1],
 																		unProcesado[2],
+																		unProcesado[11],
 																		unProcesado[3],
 																		unProcesado[4],
 																		unProcesado[5],
@@ -66,8 +79,21 @@ def get_output_string(unProcesado):
 																		unProcesado[7],
 																		unProcesado[8])
 
+def fonto_a_dic(unProcesado):
+	rta = {}
+	rta['nombre'] = unProcesado[0]
+	rta['moneda'] = unProcesado[1]
+	rta['precio'] = unProcesado[2]
+	rta['cuotapartes'] = unProcesado[9]
+	rta['monto'] = unProcesado[4]
+	rta['precio_previo'] = unProcesado[11]
+	rta['cuotapartes_previas'] = unProcesado[10]
+	rta['monto_previo'] = unProcesado[6]
+	rta['diferencia'] = unProcesado[8]
+	return rta
+
 def get_fecha_generacion():
-	return "_" + time.strftime('%Y_%m_%d')
+	return time.strftime('%Y_%m_%d')
 
 def get_archivo_salida_con_formato(archivo_salida):
 	partes = archivo_salida.split('.')
@@ -86,6 +112,7 @@ def get_parser():
 	parser.add_argument('actual', help='CVS de la ultima ronda')
 	parser.add_argument('anterior', help='CVS de la anteultima ronda')
 	parser.add_argument('--exportar', help='Vuelca el procesamiento en un archivo plano')
+	parser.add_argument('--json', help='Vuelca el procesamiento en un archivo json')
 	return parser
 
 if __name__ == '__main__':
@@ -93,13 +120,26 @@ if __name__ == '__main__':
 	
 	procesados = procesar(args.actual, args.anterior)
 	
-	if args.exportar is None:
-		for procesado in procesados:
-			print(get_output_string(procesado))
-	else:
+	if args.exportar is not None:
 		nombre_archivo_salida = get_archivo_salida_con_formato(args.exportar)
 		
 		archivo_salida = open(nombre_archivo_salida, 'w')
 		for procesado in procesados:
 			archivo_salida.write(get_output_string(procesado) + "\n")
 		archivo_salida.close()
+		
+	elif args.json is not None:
+		nombre_archivo_salida = get_archivo_salida_con_formato(args.json)
+		archivo_salida = open(nombre_archivo_salida, 'w')
+		aux = {}
+		aux['fondos'] = []
+		
+		for procesado in procesados:
+			aux['fondos'].append(fonto_a_dic(procesado))
+		
+		archivo_salida.write(json.dumps(aux, indent=4))
+		archivo_salida.close()
+		
+	else:
+		for procesado in procesados:
+			print(get_output_string(procesado))
